670905bf2ea843e57867f029915b5fd67dcd464d
jobayed24 <jkhouse117@gmail.com>
2024-06-15 21:57:58 +0600

    fs: by state could possible is it file or dir and alos file or folder states

--- fileSystem/index.js
+++ fileSystem/index.js
@@ -1,18 +1,25 @@
 const fs=require('node:fs');
 const path=require('node:path');
 const getCwb=(filename)=>path.join(__dirname,filename)
-if(fs.existsSync(getCwb('jk'))){
-    fs.rm(getCwb('jk'),{recursive:true},(err)=>{
-        if(err) throw err;
-        console.log('deleted dir successfully');
-    })
-}
+
+
+fs.stat(getCwb('jk'),(err,state)=>{
+    if(err) throw err.message;
+    console.log(state.isFile());
+})
+// if(fs.existsSync(getCwb('jk'))){
+//     fs.rm(getCwb('jk'),{recursive:true},(err)=>{
+//         if(err) throw err;
+//         console.log('deleted dir successfully');
+//     })
+// }
 
 
 // make directory
 
 
-fs.mkdir(getCwb('jk/jk/jk'),{recursive: true},(err)=>{
-    if(err) throw err.message;
-    console.log('folder created successfully');
-})
+// fs.mkdir(getCwb('jk/jk/jk'),{recursive: true},(err)=>{
+//     if(err) throw err.message;
+//     console.log('folder created successfully');
+// })
+
