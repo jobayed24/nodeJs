c035faa4b03d0dfae6d3b69c4c97cf0f654298eb
jobayed24 <jkhouse117@gmail.com>
2024-06-17 17:45:33 +0600

    fs: open file and write some data on this file without remove existing data with --Buffer state

--- fileSystem/index.js
+++ fileSystem/index.js
@@ -1,21 +1,18 @@
-const fs=require('node:fs');
+const http=require('node:http');
 const path=require('node:path');
+const fs=require('node:fs');
 const os=require('node:os');
-const getCwb=(filename)=>path.join(__dirname,filename)
 
-fs.open(getCwb('test1.txt'),'a+',(err,data)=>{
-    if(err) throw err.message;
-    if(data){
-       try{
-        fs.appendFile(data,'jobayed hossen\n','utf-8',(er)=>{
-            fs.close(data);
-            if(er) throw er;
-            
-        })
-       }catch(err){
-        fs.close(data)
-        console.log(err)
-       }
-    }
+
+let bufferData=new Buffer.from('This is jobayed hossen\n');
+
+fs.open(path.join(__dirname,"test1.txt"),'a',(err,fd)=>{
+    if(err) throw err;
+    console.log('fileDir',fd);
+    fs.write(fd,bufferData,(err,writesBytes)=>{
+        if(err) console.log("can't write to the file");
+        console.log('writes bytes',writesBytes);
+
+    })
 })
 
