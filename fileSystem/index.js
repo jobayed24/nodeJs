9964df8272faee6485d7a0b0c4fb077a8024bfb3
jobayed24 <jkhouse117@gmail.com>
2024-06-17 18:19:13 +0600

    fs: file open and write some data on it and show data on client request

--- fileSystem/index.js
+++ fileSystem/index.js
@@ -2,17 +2,26 @@ const http=require('node:http');
 const path=require('node:path');
 const fs=require('node:fs');
 const os=require('node:os');
+let increament=0;
+let server=http.createServer((req,res)=>{
+increament++;
+    
+let data=new Buffer.from(`${increament} Jobayed hossen is here \n`);
 
-
-let bufferData=new Buffer.from('This is jobayed hossen\n');
-
-fs.open(path.join(__dirname,"test1.txt"),'a',(err,fd)=>{
+fs.open(path.join(__dirname,'text.txt'),'a+',(err,fd)=>{
     if(err) throw err;
-    console.log('fileDir',fd);
-    fs.write(fd,bufferData,(err,writesBytes)=>{
-        if(err) console.log("can't write to the file");
-        console.log('writes bytes',writesBytes);
-
+    fs.write(fd,data,(err,writedBytes)=>{
+        if(err) console.log("can't possible to write data on this file");
+        console.log('total bytes',writedBytes);
+        
     })
+   fs.readFile(path.join(__dirname,'text.txt'),{encoding:'utf8'},(err,resData)=>{
+    if(err) res.end(err.message);
+
+    res.end('<h1>This is jobayed Hossen</h1>');
+   })
 })
+}
+
+).listen(3000,()=>console.log('server started'));
 
