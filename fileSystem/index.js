a83cbd3cedeccb4f3dd1c871150ff5f20bb170b7
jobayed24 <jkhouse117@gmail.com>
2024-06-20 10:49:17 +0600

    fs: openSync is return file descriptor for opening file

--- fileSystem/index.js
+++ fileSystem/index.js
@@ -2,26 +2,10 @@ const http=require('node:http');
 const path=require('node:path');
 const fs=require('node:fs');
 const os=require('node:os');
-let increament=0;
-let server=http.createServer((req,res)=>{
-increament++;
-    
-let data=new Buffer.from(`${increament} Jobayed hossen is here \n`);
 
-fs.open(path.join(__dirname,'text.txt'),'a+',(err,fd)=>{
-    if(err) throw err;
-    fs.write(fd,data,(err,writedBytes)=>{
-        if(err) console.log("can't possible to write data on this file");
-        console.log('total bytes',writedBytes);
-        
-    })
-   fs.readFile(path.join(__dirname,'text.txt'),{encoding:'utf8'},(err,resData)=>{
-    if(err) res.end(err.message);
 
-    res.end('<h1>This is jobayed Hossen</h1>');
-   })
-})
-}
+const reader=fs.openSync(path.join(__dirname,'text.txt'),'r');
 
-).listen(3000,()=>console.log('server started'));
+const dataOfFile=fs.readFileSync(reader,{encoding: 'utf8'});
 
+console.log(dataOfFile);
