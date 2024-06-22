196268353c7748858bfe46e5808910385be1e883
jobayed24 <jkhouse117@gmail.com>
2024-06-22 22:25:55 +0600

    fs.lstate helps read files and directory from file system

--- fileSystem/index.js
+++ fileSystem/index.js
@@ -2,10 +2,40 @@ const http=require('node:http');
 const path=require('node:path');
 const fs=require('node:fs');
 const os=require('node:os');
+const readline=require('node:readline');
+
+const directoryPath='./';
+
+function listFileAndTypes(dirPath){
+    fs.readdir(dirPath,(err,files)=>{
+        if(err) throw err;
+        files.forEach((file)=>{
+            fs.lstat(file,(err,fileStat)=>{
+                if(err) {
+                    console.log('there was an err with the files');
+                    return;
+                }
+                if(fileStat.isFile()){
+                    console.log(`${file} -Files`);
+                }
+                if(fileStat.isDirectory()){
+                    console.log(`${file}- Dir`);
+                }
+                if(fileStat.isSymbolicLink()){
+                    console.log(`${file}- Symbolic`)
+                }
+            })
+        })
+    })
+}
 
 
-const reader=fs.openSync(path.join(__dirname,'text.txt'),'r');
+listFileAndTypes(directoryPath);
 
-const dataOfFile=fs.readFileSync(reader,{encoding: 'utf8'});
 
-console.log(dataOfFile);
+
+
+
+
+
+
